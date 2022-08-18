623c3c258ccc07ccb7e9918a8c099048957af0b9
Pero <ynwangpeng@gmail.com>
2022-08-18 21:44:38 +0800

    Initial project
    - Initial videoSwiper, Slider, tiktok-swiper example

---
+++ examples/tiktok-swiper/pages/home/home.js
@@ -0,0 +1,54 @@
+// pages/home/home.js
+import mockData from './mockData.js';
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    list: mockData.list
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
