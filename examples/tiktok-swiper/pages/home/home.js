24743759c92ad91bc5189b9cdf19ec8a208267f2
Pero <ynwangpeng@gmail.com>
2022-09-23 17:34:38 +0800

    Update tiktok-swiper example

--- examples/tiktok-swiper/pages/home/home.js
+++ examples/tiktok-swiper/pages/home/home.js
@@ -11,6 +11,55 @@ Page({
 
   onSwiperChanged({ detail }) {
     const { item } = detail;
-    console.log(item);
+    console.log('swiper changed to: ', item);
+  },
+
+  onSwiperItemPanelLikeTap(e) {
+    const {
+      detail: { item }
+    } = e;
+    console.log('like: ', item);
+
+    const list = this.data.list.map((i) => {
+      if (i.id === item.id) {
+        return {
+          ...i,
+          hasLiked: !item.hasLiked
+        };
+      }
+      return i;
+    });
+
+    this.setData({
+      list
+    });
+  },
+
+  onSwiperItemPanelCommentTap(e) {
+    const {
+      detail: { item }
+    } = e;
+    console.log('comment: ', item);
+  },
+
+  onSwiperItemPanelShareTap(e) {
+    const {
+      detail: { item }
+    } = e;
+    console.log('share: ', item);
+  },
+
+  onSwiperItemPanelUserTap(e) {
+    const {
+      detail: { item }
+    } = e;
+    console.log('user: ', item);
+  },
+
+  onSwiperItemPanelMusicTap(e) {
+    const {
+      detail: { item }
+    } = e;
+    console.log('music: ', item);
   }
 });
